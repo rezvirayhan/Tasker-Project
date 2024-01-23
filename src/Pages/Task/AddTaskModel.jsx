6dd4a3f71c80d844c6feae299f73318d15e205f7
rezvirayhan <devrezvi.rayhan@gmail.com>
2024-01-23 12:44:32 +0600

    Modal Problem

---
+++ src/Pages/Task/AddTaskModel.jsx
@@ -0,0 +1,105 @@
+import { useState } from "react";
+
+const AddTaskModel = ({ onSave }) => {
+    const [modalTask, setModalTask] = useState({
+        title: "",
+        description: "",
+        tags: [],
+        priority: "",
+        isFavorite: false
+    })
+    const handleChange = (evt) => {
+        const name = evt.target.name;
+        let value = evt.target.value;
+        if (name === 'tags') {
+            value = value.split(",")
+        }
+        setModalTask({
+            ...modalTask,
+            [name]: value
+        })
+    }
+    return (
+        <>
+            <div className="bg-black bg-opacity-70 h-full w-full z-10 absolute top-0 left-0"></div>
+            <form
+                className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-1/4 left-1/4"
+            >
+                <h2
+                    className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]"
+                >
+                    Add New Task
+                </h2>
+                <div className="space-y-9 text-white lg:space-y-10">
+                    <div className="space-y-2 lg:space-y-3">
+                        <label htmlFor="title">Title</label>
+                        <input
+                            className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+                            type="text"
+                            name="title"
+                            id="title"
+                            value={modalTask.title}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="space-y-2 lg:space-y-3">
+                        <label htmlFor="description">Description</label>
+                        <textarea
+                            className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
+                            type="text"
+                            name="description"
+                            id="description"
+                            value={modalTask.description}
+                            onChange={handleChange}
+                            required
+                        ></textarea>
+                    </div>
+                    <div
+                        className="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20"
+                    >
+                        <div className="space-y-2 lg:space-y-3">
+                            <label htmlFor="tags">Tags</label>
+                            <input
+                                className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+                                type="text"
+                                name="tags"
+                                id="tags"
+                                value={modalTask.tags}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className="space-y-2 lg:space-y-3">
+                            <label htmlFor="priority">Priority</label>
+                            <select
+                                className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
+                                name="priority"
+                                id="priority"
+                                value={modalTask.priority}
+                                onChange={handleChange}
+                                required
+                            >
+                                <option value="">Select Priority</option>
+                                <option value="low">Low</option>
+                                <option value="medium">Medium</option>
+                                <option value="high">High</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-16 flex justify-center lg:mt-20">
+                    <button
+                        type="submit"
+                        className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
+                        onClick={() => onSave(modalTask)}
+                    >
+                        Save
+                    </button>
+                </div>
+            </form>
+        </>
+    );
+};
+
+export default AddTaskModel;
--- src/Pages/Task/TaskBoard.jsx
+++ src/Pages/Task/TaskBoard.jsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
+import AddTaskModel from "./AddTaskModel";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 const TaskBoard = () => {
+
+    const defaultTask = {
+        'id': crypto.randomUUID(),
+        'title': 'Learn React By RWS',
+        'description': 'লার্ন উইথ সুমিত একটি বাংলা প্রোগ্রামিং প্লাটফর্ম যেখানে প্রোগ্রামিং শুধু শেখানো নয়, বুঝানো হয়.',
+        'tags': ["web", "react", 'next'],
+        'priority': "High",
+        'isFavorite': true
+    }
+    const [task, setTask] = useState([defaultTask])
+    const [showAddModal, setShowAddModal] = useState(false)
+    function handleAddTask(modalTask) {
+        console.log('adding a task', modalTask);
+    }
     return (
         <section className="mb-20" id="tasks">
+            {showAddModal && <AddTaskModel onSave={handleAddTask} />}
             <div className="container">
                 <div className="p-2 flex justify-end">
                     <SearchTask></SearchTask>
                 </div>
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-                    <TaskAction></TaskAction>
-                    <TaskList></TaskList>
+                    <TaskAction onAddClick={() => setShowAddModal(true)}></TaskAction>
+                    <TaskList task={task}></TaskList>
                 </div>
             </div>
         </section>
